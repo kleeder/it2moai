f08b23d3568b0e583336e737524f638a8c7acf03
kleeder <5kleeman@informatik.uni-hamburg.de>
2022-02-02 14:21:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # it2moai
+
+- Use the example.it file to do your song.
+- Don't change samples/instruments.
+- No effects, no volume change, only notes.
+- Note Cuts cut all sounds, no matter where they are placed
+- Initial Tempo may be changed, but not the Initial Ticks.
---
+++ it2moai.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+"""
+it2moai.py, version 0.1
+----------------------
+
+Python 3 only.
+
+Use the example.it file to do your song.
+Don't change samples/instruments.
+No effects, no volume change, only notes.
+Note Cuts cut all sounds, no matter where they are placed
+Initial Tempo may be changed, but not the Initial Ticks.
+
+Version history
+---------------
+
+* 0.1: Initial creation of program.
+"""
+
+from sys import stderr, version_info
+from pytrax import impulsetracker
+import json
+
+def die(msg):
+    if isinstance(msg, BaseException):
+        msg = str(msg)
+    stderr.write(str(msg) + '\n')
+    exit(1)
+
+
+if version_info.major < 3:
+    die('python3 only!')
+
+
+MODULE = "test.it"
+SOUNDLIST = "soundlist.json"
+
+
+def get_soundlist(soundlist_file):
+    file = open(file=soundlist_file, encoding="utf8")
+    soundlist = json.load(file)
+    soundnamelist = []
+    for x in soundlist:
+        soundnamelist.append(x["name"])
+
+    return soundnamelist
+
+
+def convert(module, filename, soundnamelist):
+    try:
+        outfile = open(filename, 'w', encoding="utf8")
+    except BaseException as ex:
+        die(ex)
+
+    inittempo = (module["inittempo"])*8
+    sequence = module["orders"]
+
+    outfile.write('!speed@{}|'.format(inittempo))
+
+    for pattern_number in sequence:
+        if pattern_number != 255:
+            pattern_data = module["patterns"][pattern_number][0]
+            for row in pattern_data:
+                channel_amount = len(row)
+                if channel_amount > 0:
+                    for channel in row:
+                        channel_amount = channel_amount - 1
+                        note = channel["note"]
+                        if note == 254:
+                            outfile.write('!cut|')
+
+                        else:
+                            instrument = channel["instrument"]
+                            sample = soundnamelist[instrument-1]
+                            pitch = note-60
+                            outfile.write(sample + '@' + str(pitch) + '|')
+                            if channel_amount > 0:
+                                outfile.write('!combine|')
+                else:
+                    outfile.write('_pause|')
+
+    outfile.close()
+
+
+module = impulsetracker.parse_file(MODULE, with_patterns=True)
+soundnamelist = get_soundlist(SOUNDLIST)
+convert(module, 'output.🗿', soundnamelist)
